a0364a1b27e9b023370df7927fceda3cd4db80d1
Hiroki Kondo <kondo.matr02@gmail.com>
2020-02-17 22:31:26 +0900

    QTTextFieldの実装完了

---
+++ QiitaCommunication/UIParts/TextFields/QTTextField.swift
@@ -0,0 +1,43 @@
+//
+//  QTTextField.swift
+//  QiitaCommunication
+//
+//  Created by 近藤宏輝 on 2020/02/17.
+//  Copyright © 2020 Hiroki. All rights reserved.
+//
+
+import UIKit
+
+class QTTextField: UITextField {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    //決定したプロパティ
+    private func configure() {
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        layer.cornerRadius = 10
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.systemGray4.cgColor
+        
+        textColor = .label
+        tintColor = .label
+        textAlignment = .center
+        font = UIFont.preferredFont(forTextStyle: .title2)
+        adjustsFontSizeToFitWidth = true
+        minimumFontSize = 12
+        
+        backgroundColor = .tertiarySystemBackground
+        //自動補完をカット
+        autocorrectionType = .no
+        returnKeyType = .go
+        placeholder = "検索したい名前を記入してください"
+    }
+}
