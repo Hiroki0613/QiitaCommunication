9c477281d985ce53210debecf50378ed8aeacc70
Hiroki Kondo <kondo.matr02@gmail.com>
2020-02-18 16:58:37 +0900

    QTFollowerCellの実装

--- QiitaCommunication/UIParts/Cells/QTFollowerCell.swift
+++ QiitaCommunication/UIParts/Cells/QTFollowerCell.swift
@@ -9,5 +9,45 @@
 import UIKit
 
 class QTFollowerCell: UICollectionViewCell {
+    //reuseIDを宣言
+    static let reuseID = "QTFollowerCell"
     
+    let avatarImageView = QTAvatarImageView(frame: .zero)
+    let usernameLabel = QTTitleLabel(textAlignment: .center, fontSize: 14)
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //cellForItemAtに入れる情報
+    func set(follower: Follower) {
+        usernameLabel.text = follower.name
+    }
+    
+    
+    private func configure() {
+        addSubview(avatarImageView)
+        addSubview(usernameLabel)
+        
+        let padding:CGFloat = 8
+        
+        NSLayoutConstraint.activate([
+            avatarImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: padding),
+            avatarImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: padding),
+            avatarImageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -padding),
+            avatarImageView.heightAnchor.constraint(equalTo: avatarImageView.widthAnchor),
+            
+            usernameLabel.topAnchor.constraint(equalTo: avatarImageView.bottomAnchor, constant: 12),
+            usernameLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: padding),
+            usernameLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -padding),
+            usernameLabel.heightAnchor.constraint(equalToConstant: 20)
+        ])
+    }
 }
