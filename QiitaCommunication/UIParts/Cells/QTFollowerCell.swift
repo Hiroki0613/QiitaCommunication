06c76a33fc4fc5e907a6d09b4f11df7a91729add
Hiroki Kondo <kondo.matr02@gmail.com>
2020-02-18 16:32:27 +0900

    QTFollowerCellを追加

---
+++ QiitaCommunication/UIParts/Cells/QTFollowerCell.swift
@@ -0,0 +1,13 @@
+//
+//  QTFollowerCell.swift
+//  QiitaCommunication
+//
+//  Created by 近藤宏輝 on 2020/02/18.
+//  Copyright © 2020 Hiroki. All rights reserved.
+//
+
+import UIKit
+
+class QTFollowerCell: UICollectionViewCell {
+    
+}
