2b1945738b5fec0baeda0f80e3eee1abd1712249
Hiroki Kondo <kondo.matr02@gmail.com>
2020-02-18 13:27:53 +0900

    QTAlertVCのactionButtonの実装

--- QiitaCommunication/UIParts/Alert/QTAlertVC.swift
+++ QiitaCommunication/UIParts/Alert/QTAlertVC.swift
@@ -9,20 +9,22 @@
 import UIKit
 
 class QTAlertVC: UIViewController {
-
     //アラートを載せるviewを宣言
     let containerView = UIView()
     let titleLabel = QTTitleLabel(textAlignment: .center, fontSize: 24)
     let messageLabel = QTBodyLabel(textAlignment: .center)
     let actionButton = QTButton(backgroundColor: .systemRed, title: "戻る")
     
+    
     //initで定義される内容を宣言、情報が入らない場合を想定してオプショナル型で宣言
     var alertTitle: String?
     var message: String?
     var buttonTitle: String?
     
+    
     //共通のpadding値を宣言
     var padding:CGFloat = 20
+    
     
     init(title: String, message: String, buttonTitle: String) {
         super.init(nibName: nil, bundle: nil)
@@ -31,9 +33,11 @@ class QTAlertVC: UIViewController {
         self.buttonTitle = buttonTitle
     }
     
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -42,7 +46,9 @@ class QTAlertVC: UIViewController {
         configureContainerView()
         configureTitleLabel()
         configureMessageLabel()
+        configureAcitonButton()
     }
+    
     
     func configureContainerView() {
         view.addSubview(containerView)
@@ -61,6 +67,7 @@ class QTAlertVC: UIViewController {
         ])
     }
     
+    
     func configureTitleLabel() {
         containerView.addSubview(titleLabel)
         titleLabel.text = alertTitle ?? "情報無し"
@@ -72,6 +79,7 @@ class QTAlertVC: UIViewController {
             titleLabel.heightAnchor.constraint(equalToConstant: 30)
         ])
     }
+    
     
     func configureMessageLabel() {
         containerView.addSubview(messageLabel)
@@ -87,4 +95,21 @@ class QTAlertVC: UIViewController {
     }
     
     
+    func configureAcitonButton() {
+        containerView.addSubview(actionButton)
+        actionButton.setTitle(buttonTitle ?? "OK", for: .normal)
+        actionButton.addTarget(self, action: #selector(dismissVC), for: .touchUpInside)
+        
+        NSLayoutConstraint.activate([
+            actionButton.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: -padding),
+            actionButton.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: padding),
+            actionButton.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -padding),
+            actionButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+    
+    
+    @objc func dismissVC() {
+        dismiss(animated: true, completion: nil)
+    }
 }
