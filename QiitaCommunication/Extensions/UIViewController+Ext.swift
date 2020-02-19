321287584610a7e4899731179979bff842e622d8
Hiroki Kondo <kondo.matr02@gmail.com>
2020-02-19 21:51:50 +0900

    アクティビティーインジケータ開始を実装

--- QiitaCommunication/Extensions/UIViewController+Ext.swift
+++ QiitaCommunication/Extensions/UIViewController+Ext.swift
@@ -8,6 +8,9 @@
 
 import UIKit
 
+fileprivate var containerView: UIView!
+
+
 extension UIViewController {
     
     func presentQTAlertOnMainView(title: String,message: String, buttonTitle: String) {
@@ -17,5 +20,30 @@ extension UIViewController {
         alertVC.modalPresentationStyle = .overFullScreen
         alertVC.modalTransitionStyle = .crossDissolve
         self.present(alertVC, animated: true, completion: nil)
+    }
+    
+    //アクティビティーインジケータ開始
+    func showLoadingView() {
+        containerView = UIView(frame: view.bounds)
+        view.addSubview(containerView)
+        
+        containerView.backgroundColor = .systemBackground
+        containerView.alpha = 0
+        
+        UIView.animate(withDuration: 0.25) {
+            containerView.alpha = 0.8
+        }
+        
+        let activityIndicator = UIActivityIndicatorView(style: .large)
+        containerView.addSubview(activityIndicator)
+        
+        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            activityIndicator.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            activityIndicator.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+        
+        activityIndicator.startAnimating()
     }
 }
