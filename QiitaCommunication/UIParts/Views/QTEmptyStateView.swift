2ec35f9f9bd8bc1585a15814f00ccd9886484b8a
Hiroki Kondo <kondo.matr02@gmail.com>
2020-02-19 22:00:29 +0900

    QTEmptyStateViewを追加

---
+++ QiitaCommunication/UIParts/Views/QTEmptyStateView.swift
@@ -0,0 +1,21 @@
+//
+//  QTEmptyStateView.swift
+//  QiitaCommunication
+//
+//  Created by 近藤宏輝 on 2020/02/19.
+//  Copyright © 2020 Hiroki. All rights reserved.
+//
+
+import UIKit
+
+class QTEmptyStateView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
