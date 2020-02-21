856b7892d2cc82a9b8414fdab3a22cee55785296
Hiroki Kondo <kondo.matr02@gmail.com>
2020-02-21 23:20:26 +0900

    Userを追加

---
+++ QiitaCommunication/Model/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  QiitaCommunication
+//
+//  Created by 近藤宏輝 on 2020/02/21.
+//  Copyright © 2020 Hiroki. All rights reserved.
+//
+
+import Foundation
+
+//Userを取得するためのModel
+//参考URL
+//https://qiita.com/api/v2/users/qiita
+
+
