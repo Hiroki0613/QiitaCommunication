b07ced5b671af93eebaef15ab1a5126b3bc8b1a4
Hiroki Kondo <kondo.matr02@gmail.com>
2020-02-22 12:12:01 +0900

    QTItemInfoViewを追加

---
+++ QiitaCommunication/UIParts/Views/QTItemInfoView.swift
@@ -0,0 +1,15 @@
+//
+//  QTItemInfoView.swift
+//  QiitaCommunication
+//
+//  Created by 近藤宏輝 on 2020/02/22.
+//  Copyright © 2020 Hiroki. All rights reserved.
+//
+
+import UIKit
+
+class QTItemInfoView: UIView {
+
+ 
+
+}
