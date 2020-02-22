d5e5dd37eda8d8319f9df38b5552e04850727f69
Hiroki Kondo <kondo.matr02@gmail.com>
2020-02-22 11:11:45 +0900

    Constantsを追加

---
+++ QiitaCommunication/Model/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  QiitaCommunication
+//
+//  Created by 近藤宏輝 on 2020/02/22.
+//  Copyright © 2020 Hiroki. All rights reserved.
+//
+
+import Foundation
+
+static QTSymbols {
+    static let location = "mappin.and.ellipse"
+}
