97f5bc3276483905323e54386bd6b0c85832dec8
Hiroki Kondo <kondo.matr02@gmail.com>
2020-02-22 12:58:48 +0900

    QTItemsCountVCを追加

---
+++ QiitaCommunication/UIParts/ ViewControllers/QTItemsCountVC.swift
@@ -0,0 +1,13 @@
+//
+//  QTItemsCountVC.swift
+//  QiitaCommunication
+//
+//  Created by 近藤宏輝 on 2020/02/22.
+//  Copyright © 2020 Hiroki. All rights reserved.
+//
+
+import UIKit
+
+class QTItemsCountVC: QTItemInfoVC {
+    
+}
