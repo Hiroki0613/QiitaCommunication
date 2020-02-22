f73570df18dd9fcef0bc1884ed8ab07397ff6b6c
Hiroki Kondo <kondo.matr02@gmail.com>
2020-02-22 12:43:00 +0900

    QTItemInfoVCを追加

---
+++ QiitaCommunication/UIParts/ ViewControllers/QTItemInfoVC.swift
@@ -0,0 +1,21 @@
+//
+//  QTItemInfoVC.swift
+//  QiitaCommunication
+//
+//  Created by 近藤宏輝 on 2020/02/22.
+//  Copyright © 2020 Hiroki. All rights reserved.
+//
+
+import UIKit
+
+class QTItemInfoVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+
+}
