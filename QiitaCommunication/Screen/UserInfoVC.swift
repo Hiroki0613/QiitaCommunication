9804ff77910bcb4fa53db892871e26b772185a1a
Hiroki Kondo <kondo.matr02@gmail.com>
2020-02-22 11:45:37 +0900

    headerViewをネットワークを通して取得する機能を実装

--- QiitaCommunication/Screen/UserInfoVC.swift
+++ QiitaCommunication/Screen/UserInfoVC.swift
@@ -26,7 +26,10 @@ class UserInfoVC: UIViewController {
                 self.presentQTAlertOnMainView(title: "ユーザー名が無効です", message: errorMessage!.rawValue, buttonTitle: "OK")
                 return
             }
-            print(user)
+            DispatchQueue.main.async {
+                 self.add(childVC: QTUserInfoHeaderVC(user: user), to: self.headerView)
+            }
+           
         }
         
         layoutUI()
@@ -35,7 +38,6 @@ class UserInfoVC: UIViewController {
     
     func layoutUI() {
         view.addSubview(headerView)
-        headerView.backgroundColor = .systemPink
         headerView.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
@@ -44,7 +46,14 @@ class UserInfoVC: UIViewController {
             headerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             headerView.heightAnchor.constraint(equalToConstant: 180)
         ])
-        
+    }
+    
+    //childVCを追加
+    func add(childVC: UIViewController,to containerView: UIView) {
+        addChild(childVC)
+        containerView.addSubview(childVC.view)
+        childVC.view.frame = containerView.bounds
+        childVC.didMove(toParent: self)
     }
     
     
