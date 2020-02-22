99337679ee278cf6cc194fd58f2b19eedae08de4
Hiroki Kondo <kondo.matr02@gmail.com>
2020-02-22 12:09:40 +0900

    UserInfoVCのコードを整理

--- QiitaCommunication/Screen/UserInfoVC.swift
+++ QiitaCommunication/Screen/UserInfoVC.swift
@@ -11,68 +11,71 @@ import UIKit
 class UserInfoVC: UIViewController {
     
     let headerView = UIView()
-    
     let itemViewOne = UIView()
     let itemViewTwo = UIView()
+    var itemViews: [UIView] = []
     
     var username: String!
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureViewController()
+        layoutUI()
+        getUserInfo()
+    }
+    
+    
+    func configureViewController() {
         view.backgroundColor = .systemBackground
         let doneButton = UIBarButtonItem(barButtonSystemItem: .close, target: self, action: #selector(dismissVC))
         navigationItem.rightBarButtonItem = doneButton
-        
-        
+    }
+    
+    
+    func getUserInfo() {
         NetworkManager.shared.getUsersInfo(for: username) { (user, errorMessage) in
             guard let user = user else {
                 self.presentQTAlertOnMainView(title: "ユーザー名が無効です", message: errorMessage!.rawValue, buttonTitle: "OK")
                 return
             }
             DispatchQueue.main.async {
-                 self.add(childVC: QTUserInfoHeaderVC(user: user), to: self.headerView)
+                self.add(childVC: QTUserInfoHeaderVC(user: user), to: self.headerView)
             }
-           
         }
-        
-        layoutUI()
     }
     
     
     func layoutUI() {
-        view.addSubview(headerView)
-        view.addSubview(itemViewOne)
-        view.addSubview(itemViewTwo)
+        let padding: CGFloat = 20
+        let itemHeight: CGFloat = 140
+        itemViews = [headerView, itemViewOne, itemViewTwo]
+        
+        for itemView in itemViews {
+            view.addSubview(itemView)
+            itemView.translatesAutoresizingMaskIntoConstraints = false
+            
+            NSLayoutConstraint.activate([
+                itemView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+                itemView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding)
+            ])
+        }
         
         itemViewOne.backgroundColor = .systemPink
         itemViewTwo.backgroundColor = .systemBlue
-        
-        headerView.translatesAutoresizingMaskIntoConstraints = false
-        itemViewOne.translatesAutoresizingMaskIntoConstraints = false
-        itemViewTwo.translatesAutoresizingMaskIntoConstraints = false
-        
-        let padding: CGFloat = 20
-        let itemHeight: CGFloat = 140
-        
+  
         NSLayoutConstraint.activate([
             headerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            headerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            headerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             headerView.heightAnchor.constraint(equalToConstant: 180),
             
             itemViewOne.topAnchor.constraint(equalTo: headerView.bottomAnchor, constant: padding),
-            itemViewOne.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
-            itemViewOne.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
             itemViewOne.heightAnchor.constraint(equalToConstant: itemHeight),
             
             itemViewTwo.topAnchor.constraint(equalTo: itemViewOne.bottomAnchor, constant: padding),
-            itemViewTwo.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
-            itemViewTwo.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
             itemViewTwo.heightAnchor.constraint(equalToConstant: itemHeight)
-            
-            
         ])
     }
+    
     
     //childVCを追加
     func add(childVC: UIViewController,to containerView: UIView) {
