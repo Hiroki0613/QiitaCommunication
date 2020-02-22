09e26901fba67546be10ddbb9c2a19aa583322a3
Hiroki Kondo <kondo.matr02@gmail.com>
2020-02-22 10:46:22 +0900

    QTUserInfoHeaderVCを追加、ファイルを整理

---
+++ QiitaCommunication/UIParts/ ViewControllers/QTUserInfoHeaderVC.swift
@@ -0,0 +1,19 @@
+//
+//  QTUserInfoHeaderVC.swift
+//  QiitaCommunication
+//
+//  Created by 近藤宏輝 on 2020/02/22.
+//  Copyright © 2020 Hiroki. All rights reserved.
+//
+
+import UIKit
+
+class QTUserInfoHeaderVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+
+}
