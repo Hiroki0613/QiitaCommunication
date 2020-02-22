757242862606efc65f828f408872ac186bd7a775
Hiroki Kondo <kondo.matr02@gmail.com>
2020-02-22 10:44:14 +0900

    QTSecondaryTitleLabelを実装

--- QiitaCommunication/UIParts/Labels/QTSecondaryTitleLabel.swift
+++ QiitaCommunication/UIParts/Labels/QTSecondaryTitleLabel.swift
@@ -9,5 +9,29 @@
 import UIKit
 
 class QTSecondaryTitleLabel: UILabel {
-
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //SecondaryLabelを作成する時に自由に設定できるプロパティを定義
+    init(fontSize: CGFloat) {
+        super.init(frame: .zero)
+        font = UIFont.systemFont(ofSize: fontSize, weight: .medium)
+        configure()
+    }
+    
+    //決定しているプロパティ
+    private func configure() {
+        textColor = .secondaryLabel
+        adjustsFontSizeToFitWidth = true
+        minimumScaleFactor = 0.90
+        lineBreakMode = .byTruncatingTail
+        translatesAutoresizingMaskIntoConstraints = false
+    }
 }
