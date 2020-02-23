ea95486f8577d71697acfdee8af815657534cd3b
Hiroki Kondo <kondo.matr02@gmail.com>
2020-02-23 11:58:39 +0900

    editingStyleを実装。消去できることを確認

--- QiitaCommunication/Screen/FavoriteVC.swift
+++ QiitaCommunication/Screen/FavoriteVC.swift
@@ -93,4 +93,18 @@ extension FavoriteVC: UITableViewDelegate, UITableViewDataSource {
     }
     
     
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        guard editingStyle == .delete else { return }
+        
+        let favorite = favorites[indexPath.row]
+        favorites.remove(at: indexPath.row)
+        tableView.deleteRows(at: [indexPath], with: .left)
+        
+        PresistenceManager.updateWith(favorite: favorite, actionType: .remove) { [weak self] error in
+            guard let self = self else { return }
+            guard let error = error else { return }
+            //エラーがある場合、
+            self.presentQTAlertOnMainView(title: "消去できません", message: error.rawValue, buttonTitle: "OK")
+        }
+    }
 }
