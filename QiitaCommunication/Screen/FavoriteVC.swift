42e78a980d2a84ec578c5fb3fae2795f0b55e5e4
Hiroki Kondo <kondo.matr02@gmail.com>
2020-02-23 11:42:18 +0900

    tableViewの基本定義を実装

--- QiitaCommunication/Screen/FavoriteVC.swift
+++ QiitaCommunication/Screen/FavoriteVC.swift
@@ -10,20 +10,61 @@ import UIKit
 
 class FavoriteVC: UIViewController {
 
+    let tableView = UITableView()
+    var favorites: [Follower] = []
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBlue
-
+        configureViewController()
+        configureTableView()
+    }
+    
+    
+    func configureViewController() {
+        view.backgroundColor = .systemBackground
+        title = "お気に入り"
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    
+    func configureTableView() {
+        view.addSubview(tableView)
+        tableView.frame = view.bounds
+        tableView.rowHeight = 80
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        tableView.register(QTFavoriteCell.self, forCellReuseIdentifier: QTFavoriteCell.reuseID)
+    }
+    
+    
+    func getFavorites() {
         PresistenceManager.retrieveFavorites { (result) in
             switch result {
             case .success(let favorites):
-                print(favorites)
+                self.favorites = favorites
             case .failure(let error):
                 break
             }
         }
     }
+}
+
+
+extension FavoriteVC: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return favorites.count
+    }
     
-
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: QTFavoriteCell.reuseID, for: indexPath) as! QTFavoriteCell
+        let favorite = favorites[indexPath.row]
+        cell.set(favorite: favorite)
+        return cell
+    }
+    
+    
+    
 }
