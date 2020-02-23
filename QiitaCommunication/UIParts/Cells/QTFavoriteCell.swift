cb7bf1c17f1f9055f668d174972ca1c393085b24
Hiroki Kondo <kondo.matr02@gmail.com>
2020-02-23 11:21:28 +0900

    QTFavoriteCellを追加

---
+++ QiitaCommunication/UIParts/Cells/QTFavoriteCell.swift
@@ -0,0 +1,24 @@
+//
+//  QTFavoriteCell.swift
+//  QiitaCommunication
+//
+//  Created by 近藤宏輝 on 2020/02/23.
+//  Copyright © 2020 Hiroki. All rights reserved.
+//
+
+import UIKit
+
+class QTFavoriteCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
